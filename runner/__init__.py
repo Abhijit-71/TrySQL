7264e9970f333995967c5ce1a55a400b05588eb8
Abhijit71 <4c.abhijit71@gmail.com>
2025-12-12 11:45:46 +0530

    urls reinit properly

--- runner/__init__.py
+++ runner/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-runner = Blueprint('runner',__name__,template_folder='templates')
+runner = Blueprint('runner',__name__,template_folder='templates',url_prefix='/runner')
 
 from .routes import *
--- runner/routes.py
+++ runner/routes.py
@@ -2,11 +2,11 @@ from flask import request , jsonify , render_template
 from runner import runner
 
 
-@runner.route("/runner", methods=["POST"])
+@runner.route("/input", methods=["POST"])
 def sql_runner():
     data = request.get_json()
     return jsonify(f"ran successfully : {data}") , 200
 
-@runner.route("/hello")
-def hello():
-    return render_template('index.html')
+@runner.route("/shell")
+def runner_():
+    return render_template('shell.html')
